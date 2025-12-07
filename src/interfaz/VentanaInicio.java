1bffd7a6d830340376c14dac6a450c84f09f59e2
Carlos David Huiza Moreno <c.huiza@uniandes.edu.co>
2025-12-07 14:36:57 -0500

    ventana inicial

--- src/interfaz/VentanaInicio.java
+++ src/interfaz/VentanaInicio.java
@@ -1,9 +1,15 @@
 package interfaz;
 
+import java.awt.CardLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class VentanaInicio extends JFrame {
 	public VentanaInicio() {
+		CardLayout cl = new CardLayout();
+		setLayout(cl);
+		setSize(new Dimension(600, 300));
 		setVisible(true);
 	}
 	
