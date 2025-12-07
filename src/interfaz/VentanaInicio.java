63cf15caa3454dc9db56a1feb8c735e131eeec98
Carlos David Huiza Moreno <c.huiza@uniandes.edu.co>
2025-12-07 14:33:19 -0500

    lol

--- src/interfaz/VentanaInicio.java
+++ src/interfaz/VentanaInicio.java
@@ -1,5 +1,14 @@
 package interfaz;
 
-public class VentanaInicio {
+import javax.swing.JFrame;
 
+public class VentanaInicio extends JFrame {
+	public VentanaInicio() {
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new VentanaInicio();
+		
+	}
 }
